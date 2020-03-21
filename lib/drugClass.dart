99a3969e0f91f85bee6b58817d9a5d2fce5cd030
SK99-droid <salmakhaled708@gmail.com>
2020-03-22 00:38:00 +0200

    pharmacy class

---
+++ lib/drugClass.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Drugs{
+  String drugName;
+  String drugCode;
+  double drugPrice;
+  int drugQuantity;
+
+  Drugs({this.drugName,this.drugCode,this.drugPrice,this.drugQuantity});
+
+  //TODO: adddrugs();
+  //TODO: deletedrugs();
+  //TODO: showList();
+}
