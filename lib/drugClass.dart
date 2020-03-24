e335941830d556a94b39b8c6cdd2ea49d7b729cc
ahmedhussein100 <62433133+ahmedhussein100@users.noreply.github.com>
2020-03-24 21:14:50 +0200

    drug class
    new variable is added

--- lib/drugClass.dart
+++ lib/drugClass.dart
@@ -5,8 +5,9 @@ class Drugs{
   String drugCode;
   double drugPrice;
   int drugQuantity;
+  String date_added;
 
-  Drugs({this.drugName,this.drugCode,this.drugPrice,this.drugQuantity});
+  Drugs({this.drugName,this.drugCode,this.drugPrice,this.drugQuantity,this.date_added});
 
   //TODO: adddrugs();
   //TODO: deletedrugs();
